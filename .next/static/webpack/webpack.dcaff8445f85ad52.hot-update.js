ffa878d5ab86d6cfdd3b9ac7ea2cb7ebdb7a5b08
digitalax_ <72898465+digitalax1@users.noreply.github.com>
2022-07-14 17:58:40 -0400

    Revert "fix modules"
    This reverts commit d813cdaf57eeae56918663185ae7faeecb9e4d73.

--- .next/static/webpack/webpack.dcaff8445f85ad52.hot-update.js
+++ .next/static/webpack/webpack.dcaff8445f85ad52.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "705bb34e689267f5"; }
+/******/ 	__webpack_require__.h = function() { return "352654724799453f"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
